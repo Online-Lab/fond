f82fa6b86779a1cc34290ca7705f752fc969689d
fsanano <memonanofsa@gmail.com>
2015-06-15 15:33:59 +0300

    hover effect for map in project place

--- src/app/projects/projects.js
+++ src/app/projects/projects.js
@@ -2,8 +2,31 @@
 
 angular.module('fond.projects', [])
 
-  .controller('ProjectsCtrl', function () {
-
+  .controller('ProjectsCtrl', function ($scope) {
+    $scope.hovering = false;
+    $scope.dis1 = false;
+    $scope.dis2 = false;
+    $scope.dis3 = false;
+    $scope.dis4 = false;
+    $scope.dis5 = false;
+    $scope.dis6 = false;
+    $scope.dis7 = false;
+    $scope.dis8 = false;
+    $scope.dis9 = false;
+    $scope.dis10 = false;
+    $scope.dis11 = false;
+    $scope.dis12 = false;
+    $scope.dis13 = false;
+    $scope.dis14 = false;
+    $scope.dis15 = false;
+    $scope.dis16 = false;
+    $scope.dis17 = false;
+    $scope.dis18 = false;
+    $scope.dis19 = false;
+    $scope.dis20 = false;
+    $scope.dis21 = false;
+    $scope.dis22 = false;
+    $scope.dis23 = false;
   })
 
 ;
