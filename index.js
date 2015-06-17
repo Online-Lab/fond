a5953a336f74adc440869907a42a4dc3b53f79ea
Eugene Khashin <adastreamer@gmail.com>
2015-06-17 14:18:55 +0300

    route fixes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var app = express()
 app.set('port', (process.env.PORT || 5001))
 
 app.use(gzippo.staticGzip(__dirname + '/dist'))
+app.use("/bower_components", express.static(__dirname + "/bower_components"))
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
