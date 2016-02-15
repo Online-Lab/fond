b1d6d168db7af5b58ef7c0c0dfccaa5eece65ac3
Eugene <eugene@localhost.localdomain>
2016-02-15 14:06:45 +0300

    news update (1hr)

--- src/app/news-detail/news-detail.js
+++ src/app/news-detail/news-detail.js
@@ -2,9 +2,10 @@
 
 angular.module('fond.news-detail', [])
 
-  .controller('NewsPageCtrl', function ($scope, $stateParams, utils, getJson) {
+  .controller('NewsPageCtrl', function ($state, $rootScope, $scope, $stateParams, utils, getJson) {
     $scope.news = getJson.news;
     $scope.n = utils.findById(getJson.news, $stateParams.nId);
+    $state.current.ncyBreadcrumb.label = $scope.n.desc;
   })
 
 ;
